6c8ccc149686b51edde85feadefbfd84ad570764
xavtgai <32963566+xavtgai@users.noreply.github.com>
2021-12-16 15:45:08 +0300

    unnecessary classes removed

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -16,14 +16,14 @@ function SearchForm () {
             <form name="Search" title="search" className='search__form'> 
 
             <div className='search__query'>
-                <input className="search__field" type="text" value='' placeholder="Фильм" name="film" minLength={2} required id="title" />
+                <div className="search__field"><input type="text" value='' placeholder="Фильм" name="film" minLength={2} required id="title" /></div>
                 <button className="search__find" type="submit" id="searchButton" aria-label="поиск">Найти</button>
             </div>
             <div className='search__filter'>
             <input className='search_shortilms' id='filter' type="checkbox" checked={isChange} onChange={()=>{}} onClick= {handleToggle}>
             </input>
             <label htmlFor="filter" className='search_shortilms_label'></label>
-            {/* <button className="switch-btn switch-on"></button> */}
+            
             <p className='search__filter_title'>Короткометражки</p>
             </div>
 
